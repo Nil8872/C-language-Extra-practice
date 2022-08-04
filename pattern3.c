66e40c477ef03fb0893b4bb859f64e13d539efe9
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-08-05 00:43:37 +0530

    Pattern 3 is added

---
+++ pattern3.c
@@ -0,0 +1,44 @@
+# include<stdio.h>
+
+int main(){
+    int n =5;
+    for(int i = 1 ; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            printf("* ");
+        }
+        for(int k = i; k<n; k++){
+            printf("  ");
+        }
+        for(int j=i; j<n-1;j++){
+            printf("  ");
+        }
+        for(int k = 1; k<=i; k++){
+            if(k==n){
+                printf(" ");
+            }
+            else{
+                
+            printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    for(int i=1; i<n; i++){
+        for(int j=i; j<n; j++){
+            printf("* ");
+        }
+        for(int k = 1; k<=i; k++){
+            printf("  ");
+        }
+        for(int k=1; k<i; k++){
+            printf("  ");
+        }
+
+        for(int j=i; j<n; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
